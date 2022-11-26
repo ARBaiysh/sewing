7a600797f2684b9eaca0a39ce07ca2818a7894a9
Workstation <o0052ib@gmail.com>
2022-11-26 17:55:30 +0600

    add StorehouseController

---
+++ src/main/java/kg/ssb/sewing/controller/StorehouseController.java
@@ -0,0 +1,32 @@
+package kg.ssb.sewing.controller;
+
+import kg.ssb.sewing.rest.Rest1cClientStorehouse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/storehouse")
+@Slf4j
+@RequiredArgsConstructor
+@PreAuthorize(value = "hasAnyRole('MASTER','SEAMSTRESS','MASTERS_LEADER','ROLE_HEAD_OF_CUTTING')")
+@CrossOrigin
+public class StorehouseController {
+
+    private final Rest1cClientStorehouse rest1cClientStorehouse;
+
+
+    @GetMapping()
+    public ResponseEntity<Object> getStorehouse() {
+        log.info("Start getStorehouse");
+        ResponseEntity<Object> storehouse = rest1cClientStorehouse.getStorehouse();
+        log.info("Finish getStorehouse response status-{}", storehouse.getStatusCode());
+        return new ResponseEntity<>(storehouse.getBody(), HttpStatus.OK);
+    }
+}
