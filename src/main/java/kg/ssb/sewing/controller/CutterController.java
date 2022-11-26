ec4bc5feb2a96fc5b6205938d9c7286736d6a2df
Workstation <o0052ib@gmail.com>
2022-11-26 12:25:55 +0600

    add CutterController

--- src/main/java/kg/ssb/sewing/controller/CutterController.java
+++ src/main/java/kg/ssb/sewing/controller/CutterController.java
@@ -1,5 +1,6 @@
 package kg.ssb.sewing.controller;
 
+import kg.ssb.sewing.rest.Rest1cClientCutter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -8,18 +9,18 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/balance")
+@RequestMapping("/api/cutter")
 @Slf4j
 @RequiredArgsConstructor
 @PreAuthorize(value = "hasAnyRole('MASTER','SEAMSTRESS','MASTERS_LEADER')")
 @CrossOrigin
-public class BalanceController {
+public class CutterController {
 
-    private final BalanceService balanceService;
+    private final Rest1cClientCutter rest1cClientCutter;
 
 
     @GetMapping("/{storehouseId}")
-    public ResponseEntity<Object> getBalanceByStorehouseId(@PathVariable String storehouseId) {
-        return new ResponseEntity<>(balanceService.getBalanceByStorehouseId(storehouseId), HttpStatus.OK);
+    public ResponseEntity<Object> getCutterByStorehouseId(@PathVariable String storehouseId) {
+        return new ResponseEntity<>(rest1cClientCutter.getCutterByStorehouseId(storehouseId).getBody(), HttpStatus.OK);
     }
 }
---
+++ src/main/java/kg/ssb/sewing/rest/Rest1cClientCutter.java
@@ -0,0 +1,17 @@
+package kg.ssb.sewing.rest;
+
+import kg.ssb.sewing.rest.config.Rest1cConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+@FeignClient(value = "Rest1cClientCutter", url = "${base1c.url}", configuration = Rest1cConfig.class)
+public interface Rest1cClientCutter {
+    @RequestMapping(method = RequestMethod.GET, value = "cutter?storehouseId", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Object> getCutterByStorehouseId(@RequestParam("storehouseId") String storehouseId);
+}
